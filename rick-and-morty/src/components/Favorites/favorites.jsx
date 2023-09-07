0edc48e34f2aee41196523a61cb036b9eaad1ca1
Sunny1606 <solescobares@gmail.com>
2023-09-07 15:21:52 -0300

    done

--- rick-and-morty/src/components/Favorites/favorites.jsx
+++ rick-and-morty/src/components/Favorites/favorites.jsx
@@ -1,28 +1,54 @@
-import style from './favorite.module.css'
-import { connect , useSelector} from "react-redux";
+import style from "./favorite.module.css";
+import { connect, useDispatch, useSelector } from "react-redux";
 import Card from "../Card/Card";
+import { filterCards, orderCards } from "../../redux/actions";
+import { useState } from "react";
 
 // eslint-disable-next-line react-refresh/only-export-components
 const Favorites = () => {
-  // const { myFavorites } = props;
-const myFavorites= useSelector(state=>state.myFavorites)
+  const dispatch = useDispatch();
+  const [aux, setAux] = useState(false);
+
+  const handleOrder = (e) => {
+    dispatch(orderCards(e.target.value));
+    setAux(!aux)
+  };
+
+  const handleFilter = (e) => {
+    dispatch(filterCards(e.target.value));
+  };
+ 
+  const myFavorites = useSelector((state) => state.myFavorites);
   return (
-  <div className={style.favoriteContainer}>
-    {myFavorites.map((character)=> {
-    return (
-        <Card
-        key={character.id}
-        id={character.id}
-        name={character.name}
-        species={character.species}
-        gender={character.gender}
-        origin={character.origin}
-        image={character.image} 
-        />
-    )
-  })}
-  </div>
-  )
+    <div className={style.favoriteContainer}>
+      <div>
+        <select onChange={handleOrder}>
+          <option value="A">ASCENDENTE</option>
+          <option value="D">DESCENDENTE</option>
+        </select>
+        <select onChange={handleFilter}>
+          <option value="Male">Male</option>
+          <option value="Famale">Famale</option>
+          <option value="Genderless">Genderless</option>
+          <option value="unknown">unknown</option>
+        </select>
+      </div>
+
+      {myFavorites.map((character) => {
+        return (
+          <Card
+            key={character.id}
+            id={character.id}
+            name={character.name}
+            species={character.species}
+            gender={character.gender}
+            origin={character.origin}
+            image={character.image}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
--- rick-and-morty/src/redux/actions.js
+++ rick-and-morty/src/redux/actions.js
@@ -1,13 +1,26 @@
 export const addFav = (character) => {
-    return {
-        type: "ADD_FAV",
-        payload: character
-    }
-}
+  return {
+    type: "ADD_FAV",
+    payload: character,
+  };
+};
 
 export const removeFav = (id) => {
-    return {
-        type: "REMOVE_FAV",
-        payload: id
-    }
-}
+  return {
+    type: "REMOVE_FAV",
+    payload: id,
+  };
+};
+
+export const filterCards = (gender) => {
+  return {
+    type: "FILTER",
+    payload: gender,
+  };
+};
+export const orderCards = (orden) => {
+  return {
+    type: "ORDER",
+    payload: orden,
+  };
+};
